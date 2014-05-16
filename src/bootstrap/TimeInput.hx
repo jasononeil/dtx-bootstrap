8fce0d844d723c3226d12567c30301a62b60c3a8
Jason O'Neil <jason.oneil@gmail.com>
2014-05-16 16:03:49 +1000

    On FF29+ having input type="number" breaks this class

--- src/bootstrap/TimeInput.hx
+++ src/bootstrap/TimeInput.hx
@@ -10,6 +10,7 @@ class TimeInput extends NumberInput
 	public function new(?min:TimeOfDay=0, ?max:TimeOfDay=86400, ?step:TimeOfDay=300, ?start:TimeOfDay=null, ?placeholder="")
 	{
 		super(min, max, step, start, placeholder);
+		this.setAttr("type","text");
 	}
 
 	override function format(v:Float):String {
