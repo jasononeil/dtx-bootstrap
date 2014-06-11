b78eac640451ef8a3b5da5c2353c3f81ce898042
Jason O'Neil <jason.oneil@gmail.com>
2014-06-11 13:37:52 +0800

    Fix glitch in TimeInput, working correctly now.

--- src/bootstrap/TimeInput.hx
+++ src/bootstrap/TimeInput.hx
@@ -1,6 +1,7 @@
 package bootstrap;
 
 using Detox;
+using Dates;
 
 @:skipTemplating
 class TimeInput extends NumberInput
@@ -12,11 +13,25 @@ class TimeInput extends NumberInput
 	}
 
 	override function format(v:Float):String {
-		var timestamp = DateTools.seconds( v );
-		return DateTools.format( Date.fromTime(timestamp), "%r" );
+		var timestamp = Date.now().getTime().snap(Day,Down)+(v*1000);
+		var date = Date.fromTime(timestamp);
+		return date.format([ "C", "%I:%M%p" ]);
 	}
 
+	var parseRegex = ~/([012]?\d):([0-5]\d) ?(PM|AM)/;
+
 	override function parse(v:String):Float {
-		return Date.fromString( v ).getTime() / 1000;
+		if ( parseRegex.match(v.toUpperCase()) ) {
+			var hour = Std.parseInt( parseRegex.matched(1) );
+			var min = Std.parseInt( parseRegex.matched(2) );
+			switch [hour, parseRegex.matched(3)] {
+				case [12,"AM"]: hour = 0;
+				case [12,"PM"]: hour = 12;
+				case [_,"PM"]: hour += 12;
+				default:
+			}
+			return hour*3600 + min*60;
+		}
+		throw 'Failed to parse date: $v. Format must be HH:MM(PM/AM)';
 	}
 }
